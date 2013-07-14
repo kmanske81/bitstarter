0db1c0e5d9d586d3c7e3022daa2d8d23be6762c2
Ubuntu <ubuntu@ip-10-151-43-7.ec2.internal>
2013-07-14 09:56:10 +0000

    adjustment

--- web.js
+++ web.js
@@ -1,11 +1,11 @@
 var express = require('express');
-var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var content = fs.readFile("index.html", encoding='utf8');
-  response.send(content);
+  var fs = require('fs');
+  var buffer = new Buffer();
+  response.send(buffer.toString('utc-8', fs.readFileSync("index.html")));
 });
 
 var port = process.env.PORT || 5000;
